aac291d9f4ba4fa8cece06979671b6ffc9e680ea
Rafael <grafa74@gmail.com>
2022-11-18 20:22:33 +0100

    added d¡bconnect

---
+++ app.js
@@ -0,0 +1,38 @@
+import express from 'express';
+import cors from 'cors';
+import morgan from 'morgan';
+import { tableRouter } from './router/table.js';
+import { userRouter } from './router/user.js';
+export const app = express();
+app.disable("x-powered-by");
+const corsOptions = {
+    origin: '*',
+};
+app.use(morgan('dev'));
+app.use(cors(corsOptions));
+app.use(express.json());
+app.use((req, res, next) => {
+    const origin = req.header('Origin') || '*';
+    res.setHeader('Access-Control-Allow-Origin', origin);
+    next();
+});
+app.use('/tables', tableRouter);
+app.use('/users', userRouter);
+app.get('/', (req, res) => {
+    res.send('API express de products').end();
+});
+app.use((error, _req, resp, next) => {
+    console.log(error.name, error.statusCode, error.statusMessage, error.message);
+    //el mensaje de aqui arriba es para nuestro tecnico
+    let status = error.statusCode || 500;
+    if (error.name === 'Validation Error') {
+        status = 406;
+    }
+    //el mensaje que sigue aqui abajo es el que recibe nuestro usuario.
+    const result = {
+        status: status,
+        type: error.name,
+        error: error.message,
+    };
+    resp.status(status).json(result).end();
+});
